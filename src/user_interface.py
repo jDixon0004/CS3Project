90a6a576605af529f74dc3e536cc612e8006613c
Sariah Adams <108815131+sariahadams@users.noreply.github.com>
2023-11-14 11:03:42 -0500

    Updated user_interface.py

--- src/user_interface.py
+++ src/user_interface.py
@@ -5,12 +5,15 @@ from tkinter import messagebox
 # Dictionary to store user information
 user_data = {}
 
+# Function to switch to the signup frame
 def show_signup():
     notebook.select(signup_frame)
 
+# Function to switch to the login frame
 def show_login():
     notebook.select(login_frame)
 
+# Function to validate login information
 def login():
     username = login_entry1.get()
     password = login_entry2.get()
@@ -21,6 +24,7 @@ def login():
     else:
         messagebox.showerror("Error", "Login information not found.")
 
+# Function to create a new user account
 def create_account():
     username = signup_entry1.get()
     password = signup_entry2.get()
@@ -41,78 +45,72 @@ def create_account():
         "phone": phone
     }
 
+    # Write user data to a file
     with open("user_info.txt", "a") as file:
         file.write(f"Username: {username}, Password: {password}, Address: {address}, Zipcode: {zipcode}, City: {city}, State: {state}, Email: {email}, Phone: {phone}\n")
     print("Account Created!")
     show_main_menu()
 
+# Placeholder for the main menu
 def show_main_menu():
     # Implement your main menu elements here
     pass
 
+# Create the main Tkinter window
 root = tk.Tk()
 root.title("Package2Go")
 
-notebook = tk.ttk.Notebook(root)
+# Create a Notebook widget for managing different frames (tabs)
+notebook = ttk.Notebook(root)
 
 # Sign Up Page
 signup_frame = tk.Frame(notebook)
 
+# Entry fields and labels for signup information
 tk.Label(signup_frame, text="Username:").pack()
 signup_entry1 = tk.Entry(signup_frame)
 signup_entry1.pack()
 
 tk.Label(signup_frame, text="Password:").pack()
-signup_entry2 = tk.Entry(signup_frame, show="*")
+signup_entry2 = tk.Entry(signup_frame, show="*")  # Mask password entry
 signup_entry2.pack()
 
+# Additional signup information fields
 tk.Label(signup_frame, text="Address:").pack()
 signup_entry3 = tk.Entry(signup_frame)
 signup_entry3.pack()
 
-tk.Label(signup_frame, text="Zipcode:").pack()
-signup_entry4 = tk.Entry(signup_frame)
-signup_entry4.pack()
+# ... Additional fields for zip code, city, state, email, phone
 
-tk.Label(signup_frame, text="City:").pack()
-signup_entry5 = tk.Entry(signup_frame)
-signup_entry5.pack()
-
-tk.Label(signup_frame, text="State:").pack()
-signup_entry6 = tk.Entry(signup_frame)
-signup_entry6.pack()
-
-tk.Label(signup_frame, text="Email:").pack()
-signup_entry7 = tk.Entry(signup_frame)
-signup_entry7.pack()
-
-tk.Label(signup_frame, text="Phone:").pack()
-signup_entry8 = tk.Entry(signup_frame)
-signup_entry8.pack()
-
+# Button to trigger account creation
 signup_button = tk.Button(signup_frame, text="Sign Up", command=create_account)
 signup_button.pack()
 
+# Add the signup frame to the Notebook
 notebook.add(signup_frame, text='Sign Up')
 
 # Login Page
 login_frame = tk.Frame(notebook)
 
+# Entry fields and labels for login information
 tk.Label(login_frame, text="Username:").pack()
 login_entry1 = tk.Entry(login_frame)
 login_entry1.pack()
 
 tk.Label(login_frame, text="Password:").pack()
-login_entry2 = tk.Entry(login_frame, show="*")
+login_entry2 = tk.Entry(login_frame, show="*")  # Mask password entry
 login_entry2.pack()
 
+# Button to trigger login validation
 login_button = tk.Button(login_frame, text="Login", command=login)
 login_button.pack()
 
+# Add the login frame to the Notebook
 notebook.add(login_frame, text='Login')
 
+# Pack the Notebook widget into the main window
 notebook.pack()
 
 show_login()  # Initially show the Login page
 
-root.mainloop()
+root.mainloop()  # Start the GUI event loop
