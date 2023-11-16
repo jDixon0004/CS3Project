1bac7d57418daa413843166a28f7ed72ba8d2ad6
JaelenDixon <113641100+jDixon0004@users.noreply.github.com>
2023-11-15 22:44:34 -0500

    Added database implementation, id generation, and some button functionality
    Did not add functionality for update_package_status and view_all_active_orders

--- src/PackageApp.py
+++ src/PackageApp.py
@@ -1,7 +1,13 @@
 import tkinter as tk
+import os
 from tkinter import messagebox
 from AdminUserInterface import AdminUserInterface
 from CustomerPackage import CustomerPackage
+from CustomerDatabase import CustomerDatabase
+from PackageDatabase import PackageDatabase
+
+package_db = PackageDatabase(os.path.join(os.path.dirname(__file__), '..', 'data/package_database.txt'))
+customer_db = CustomerDatabase(os.path.join(os.path.dirname(__file__), '..', 'data/user_database.txt'))
 
 class PackageApp:
     def __init__(self, master):
@@ -10,17 +16,17 @@ class PackageApp:
 
         # CustomerPackage Input Fields
         tk.Label(master, text='Customer ID').grid(row=0, column=0)
-        tk.Label(master, text='Package ID').grid(row=1, column=0)
+        #tk.Label(master, text='Package ID').grid(row=1, column=0)
         tk.Label(master, text='Recipient Name').grid(row=2, column=0)
         tk.Label(master, text='Recipient Address').grid(row=3, column=0)
 
         self.customer_id_entry = tk.Entry(master)
-        self.package_id_entry = tk.Entry(master)
+        #self.package_id_entry = tk.Entry(master)
         self.recipient_name_entry = tk.Entry(master)
         self.recipient_address_entry = tk.Entry(master)
 
         self.customer_id_entry.grid(row=0, column=1)
-        self.package_id_entry.grid(row=1, column=1)
+        #self.package_id_entry.grid(row=1, column=1)
         self.recipient_name_entry.grid(row=2, column=1)
         self.recipient_address_entry.grid(row=3, column=1)
 
@@ -40,20 +46,29 @@ class PackageApp:
 
     def create_package(self):
         customer_id = self.customer_id_entry.get()
-        package_id = self.package_id_entry.get()
+        #package_id = self.package_id_entry.get()
         recipient_name = self.recipient_name_entry.get()
         recipient_address = self.recipient_address_entry.get()
         
-        new_package = CustomerPackage(customer_id, package_id, recipient_name, recipient_address)
-        messagebox.showinfo("Success", "Package Created Successfully")
+        if customer_db.get_user(id=customer_id) is not None:
+            package_id = package_db.create_package(customer_id, recipient_name, recipient_address)
+            #new_package = CustomerPackage(customer_id, package_id, recipient_name, recipient_address)
+            messagebox.showinfo("Success", "Package Created Successfully")
+        else:
+            messagebox.showinfo("Failure", "Customer With Given ID Does Not Exist")
 
         # Optionally store or process new_package as needed
 
     def assign_package(self):
         package_id = self.admin_package_id_entry.get()
         # Retrieve the package instance by package_id and assign it
-        # self.admin_interface.assign_package(retrieved_package)
-        messagebox.showinfo("Success", "Package Assigned Successfully")
+        package_dict = package_db.retrieve_package(package_id)
+        if package_dict is not None:
+            retrieved_package = CustomerPackage(package_dict=package_dict)
+            self.admin_interface.assign_package(retrieved_package)
+            messagebox.showinfo("Success", "Package Assigned Successfully")
+        else:
+            messagebox.showinfo("Failure", "Invalid Package ID")
 
     def update_package_status(self):
         # Similar implementation as assign_package
@@ -61,7 +76,12 @@ class PackageApp:
 
     def cancel_order(self):
         # Similar implementation as assign_package
-        pass
+        package_id = self.admin_package_id_entry.get()
+        db_response = package_db.update_status(package_id, "Cancelled")
+        if db_response:
+            messagebox.showinfo("Success", "Order Cancelled Successfully")
+        else:
+            messagebox.showinfo("Failure", "Invalid Package ID")
 
     def view_all_active_orders(self):
         # Implement logic to view all active orders
