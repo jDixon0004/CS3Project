89b73074edb441d1fcd43ce9fae6b096e55027aa
JaelenDixon <113641100+jDixon0004@users.noreply.github.com>
2023-11-15 22:41:41 -0500

    Added construction from dictionary

--- src/CustomerPackage.py
+++ src/CustomerPackage.py
@@ -1,10 +1,19 @@
 class CustomerPackage:
-    def __init__(self, customer_id, package_id, recipient_name, recipient_address, status='Order Received'):
-        self.customer_id = customer_id
-        self.package_id = package_id
-        self.recipient_name = recipient_name
-        self.recipient_address = recipient_address
-        self.status = status
+    def __init__(self, customer_id=None, package_id=None, recipient_name=None, recipient_address=None, status='Order Received', package_dict=None):
+        if package_dict is not None:
+            self.customer_id = package_dict['customer_id']
+            self.package_id = package_dict['package_id']
+            self.recipient_name = package_dict['recipient_name']
+            self.recipient_address = package_dict['recipient_address']
+            self.status = package_dict['status']
+        
+        else:
+            self.customer_id = customer_id
+            self.package_id = package_id
+            self.recipient_name = recipient_name
+            self.recipient_address = recipient_address
+            self.status = status
+        
 
     def update_order_details(self, new_name, new_address):
         self.recipient_name = new_name
